362bb5ea157206d0163f965c09b9590068c2bd7e
ZakariaZack98 <155935860+ZakariaZack98@users.noreply.github.com>
2024-11-25 11:46:10 +0600

    performance optimized by concurrently running fetch requests

--- js/main.js
+++ js/main.js
@@ -279,22 +279,9 @@ async function getWeather(cityName) {
 
 // fetching daily forecast from AccuWeather========================
 async function getForecastAW(cityName) {
-  let locationKey;
-  async function getLocationKey() {
-    const response = await fetch(
-      `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${AW_apiKey}&q=${cityName}`
-    );
-    if (!response.ok) throw new Error(`Error: ${response.status}`);
-    const data = await response.json();
-    locationKey = data[0].Key;
-  }
-  await getLocationKey();
-  const response = await fetch(
-    `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${AW_apiKey}&details=true&metric=true`
-  );
-  if (!response.ok) throw new Error(`Error: ${response.status}`);
-  const data = await response.json();
-  forecastDataAW = data;
+  let locationData = await fetchData(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${AW_apiKey}&q=${cityName}`);
+  let locationKey = locationData[0].Key;
+  forecastDataAW = await fetchData(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${AW_apiKey}&details=true&metric=true`);
 }
 
 // data update call======================================
